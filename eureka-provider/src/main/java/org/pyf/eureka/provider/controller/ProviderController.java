b01991d9c10c760b328d0363cef215d26440c5dd
hanqf <hanqf2008@163.com>
2018-10-26 17:22:01 +0800

    增加cloud-config

--- eureka-provider/src/main/java/org/pyf/eureka/provider/controller/ProviderController.java
+++ eureka-provider/src/main/java/org/pyf/eureka/provider/controller/ProviderController.java
@@ -16,11 +16,17 @@ public class ProviderController {
     @Value("${server.port}")
     private String port;
 
+    @Value("${config.param.d1:Nd1}")
+    private String d1;
+
+    @Value("${config.param.d2:Nd2}")
+    private String d2;
+
 
     @ApiOperation(value = "获取服务器信息",notes = "根据传入名称打印服务器信息")
     @GetMapping("/getinfo")
     public String getInfo(String name){
-        return "Hi,"+name+" , port is:" + port;
+        return "Hi,"+name+" , port is:" + port + " , d1==" + d1 + " ,d2==" + d2;
     }
 
     @ApiOperation(value = "获取服务器信息2",notes = "根据传入名称打印服务器信息2")
